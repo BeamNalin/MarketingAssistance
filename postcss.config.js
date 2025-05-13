1a9a896e323de6caad0e0de19bb652a4b11870aa
Nalin Amornratthamrong <122557038+BeamNalin@users.noreply.github.com>
2025-05-13 18:44:22 +0000

    tallwin

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,1 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+export const plugins = [require("tailwindcss"), require("autoprefixer")];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // รวมไฟล์ทั้งหมดใน src
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+export const content = [
+  "./src/**/*.{html,js,ts,jsx,tsx}", // Adjust this to match your project structure
+];
+export const theme = {
+  extend: {},
 };
+export const plugins = [];
